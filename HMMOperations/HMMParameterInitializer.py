87976ffa06c20a5d854c59eb78179ae3609eebd7
Paul O'Neil <redballoon36@gmail.com>
2014-05-12 01:24:33 -0400

    Fix HMM formatting

--- HMMOperations/HMMParameterInitializer.py
+++ HMMOperations/HMMParameterInitializer.py
@@ -3,26 +3,24 @@
 
 # remember parameters keys are (x,y) for p(x|y)
 
-def initializeParameters(hiddenStateList,observationList):
-    return initializeUniformDistribution(hiddenStateList,observationList)
+def initializeParameters(hiddenStateList, observationList):
+    return initializeUniformDistribution(hiddenStateList, observationList)
 
 
 def initializeUniformDistribution(hiddenStateList, observationList):
-    uniformEmissionProbability = float(1)/float(len(observationList))
-    uniformTransitionProbability = float(1)/float(len(hiddenStateList))
+    uniformEmissionProbability = float(1) / float(len(observationList))
+    uniformTransitionProbability = float(1) / float(len(hiddenStateList))
     emissionProbabilityDict = {}
     transitionProbabilityDict = {}
 
     for hiddenState in hiddenStateList:
         for observation in observationList:
-            emissionProbabilityDict[(observation,hiddenState)] = uniformEmissionProbability
+            emissionProbabilityDict[(observation, hiddenState)] = uniformEmissionProbability
 
     for hiddenState in hiddenStateList:
         for otherHiddenState in hiddenStateList:
-            transitionProbabilityDict[(otherHiddenState,hiddenState)] = uniformTransitionProbability
-        transitionProbabilityDict[(hiddenState,"<s>")] = uniformTransitionProbability # <s> is our initial state
-        transitionProbabilityDict[("</s>",hiddenState)] = uniformTransitionProbability # </s> is our final state
-
-
+            transitionProbabilityDict[(otherHiddenState, hiddenState)] = uniformTransitionProbability
+        transitionProbabilityDict[(hiddenState, "<s>")] = uniformTransitionProbability # <s> is our initial state
+        transitionProbabilityDict[("</s>", hiddenState)] = uniformTransitionProbability # </s> is our final state
 
     return emissionProbabilityDict,transitionProbabilityDict
